5fd6be627373c3d859f18bd20d24e9a0c43c09b8
Geert Theys <geert.theys@gmail.com>
2018-05-08 09:45:01 +0700

    add cookie information for auth to timeline

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+)
+
+type cookie struct {
+	SID     string
+	HSID    string
+	SSID    string
+	APISID  string
+	SAPISID string
+	NID     string
+	JAR     string
+}
 
 func main() {
+	var config cookie
+	if _, err := toml.DecodeFile("cookie.toml", &config); err != nil {
+		fmt.Println(err)
+		return
+	}
 	fmt.Println("Start fetching timeline")
+	fmt.Println("Reading Cookie info:")
+	fmt.Printf("SID: %s\nHSID: %s\nSSID: %s\nAPISID: %s\nSAPISID: %s\nNID: %s\nJAR: %s\n",
+		config.SID, config.HSID, config.SSID, config.APISID, config.SAPISID,
+		config.NID, config.JAR)
+
 }
